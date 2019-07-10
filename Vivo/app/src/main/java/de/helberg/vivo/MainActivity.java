d54d5671332373772492f160ce4a8fec8a76c7a0
adrian-helberg <adrianbostelmann@gmx.de>
2019-07-10 22:10:22 +0200

    Create Project, Learning Class

---
+++ Vivo/app/src/main/java/de/helberg/vivo/MainActivity.java
@@ -0,0 +1,19 @@
+package de.helberg.vivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * MainActivity
+ *
+ * For learning about android app programming
+ * @author Adrian Helberg
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
